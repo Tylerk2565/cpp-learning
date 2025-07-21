5fe3cdb4fc588aa852823f00f4e9c50e89300e6e
Tylerk2565 <176984154+Tylerk2565@users.noreply.github.com>
2025-07-21 17:13:34 -0400

    First Hello World!

---
+++ basics/01-hello-world/hello-world.cpp
@@ -0,0 +1,14 @@
+#include <iostream> // Includes input/output stream library
+
+int main() {
+  std::cout << "Hello World!" << std::endl; // Print to console
+  return 0; 
+}
+
+
+// #include <iostream> pulls in functionality to output text to screen (std::cout)
+// main() - the entry point of every c++ program 
+// std::cout - outputs text; the std:: means it's part of the standard library 
+// return 0; - indicates the program ran successfully
+
+// With game dev std::cout is used early on for debugging game objects, printing player positions and displaying simple text UIs before using graphics

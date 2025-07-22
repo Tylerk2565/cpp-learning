0ab64fe1e29f30ef4a13b3feaa2a996d0d42ae1b
Tylerk2565 <176984154+Tylerk2565@users.noreply.github.com>
2025-07-22 13:38:07 -0400

    if-else statment example

---
+++ basics/03-conditionals-loops/if-else.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int health = 0;
+
+  if (health > 0) {
+    cout << "Player is alive.\n";
+  } else {
+    cout << "Game Over!\n";
+  }
+
+  return 0;
+}
